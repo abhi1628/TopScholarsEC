8542b50d9ef0bf0168a9e9e6038a11c37e33c0d3
abhi1628 <55579968+abhi1628@users.noreply.github.com>
2021-12-12 10:52:20 +0530

    Create GreaterEqualLesser.py
    The python program checks if the 1st number is greater than or smaller than the 2nd number or both the numbers are equal.

---
+++ GreaterEqualLesser.py
@@ -0,0 +1,8 @@
+a = int(input('Enter the 1st number:'))
+b = int(input('Enter the 2nd number:'))
+if a>b:
+  print(a,'is the greater number')
+elif a == b:
+  print('Both the numbers are equal')
+else:
+  print(b,'is the greater number')
