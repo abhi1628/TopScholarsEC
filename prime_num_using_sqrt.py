23ed2aedca29b06e5bd52256bea6d0ebecaed814
Abhishek Singh <abhishek.ec@global.org.in>
2022-09-07 13:01:10 +0530

    Update prime_num_using_sqrt.py

--- prime_num_using_sqrt.py
+++ prime_num_using_sqrt.py
@@ -1,10 +1,13 @@
 import math
 from math import sqrt
 a = int(input('Enter the number:'))
+count = 0
 for i in range(2, 1 + int(sqrt(a))):
+    count += 1
     if a % i == 0:
         print(a,'is not prime because',
               i,'x',a//i,'=',a)
         break
 else:
     print(a,'is prime')
+print('The no.of iterations are:',count)
